10fe221052e04c30735c9a971ce4bf67414ae826
lorak12 <karolstroz34@gmail.com>
2022-03-26 09:34:44 +0100

    Poprawiony ProgressBar

--- ProgressBar/progressbar.js
+++ ProgressBar/progressbar.js
@@ -1,8 +1,10 @@
-document.addEventListener("scroll", ()=>{
-    
-    
-    let bar = document.querySelector("div");
-    let wysokosc = window.scrollY;
-    bar.style.width = ((wysokosc / 10) + "%");
-    console.log(bar.style.width);
+document.addEventListener("DOMContentLoaded", ()=> {
+    document.addEventListener("scroll", ()=>{
+        let bar = document.querySelector("div");
+        let wysokosc = document.querySelector("main").offsetHeight;
+        let scrollSize = window.scrollY;
+        let windowHeight = window.innerHeight;
+        let procent = (scrollSize/(wysokosc - windowHeight)*100);
+        bar.style.width = procent + "%";
+    });
 });
